cb5ddb06ad3a21847f33fd887bf02cd1162c0955
Shanu Flash <34480644+shanuflash@users.noreply.github.com>
2018-04-18 13:49:59 +0530

    Add files via upload

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,23 +1,4 @@
-This repository contains device configuration for Xiaomi Redmi 5 Plus/Redmi Note 5 (vince)
-
-Basic   | Spec Sheet
--------:|:----------
-CPU     | Octa-core 2.02 GHz ARM Cortex A53
-Chipset | Qualcomm Snapdragon 625, MSM8953
-GPU     | Adreno 506
-ROM     | 32/64GB 
-RAM     | 3/4GB
-Android | 7.1.2
-Battery | 4000 mAh
-Display | 1080x2160 pixels, 5.99
-Rear Camera  | 12MP, PDAF
-Front Camera | 5 MP
-
-![Xiaomi Redmi 5 Plus](https://cdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-5-plus-2.jpg "Xiaomi Redmi 5 Plus")
-
-## Copyright
-
-```
+#!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
@@ -34,3 +15,35 @@ Front Camera | 5 MP
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+set -e
+
+DEVICE=vince
+VENDOR=xiaomi
+
+INITIAL_COPYRIGHT_YEAR=2017
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
+
+# Finish
+write_footers
